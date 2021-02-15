28eb5e99059bcf80c618d1d27e11409935842fff
pseudodream <dian_zhaoa@163.com>
2021-02-16 02:29:37 +0800

    First Commit

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -8,11 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var wordsRemainingLabel: UILabel!
+    @IBOutlet weak var wordsGuessedLabel: UILabel!
+    @IBOutlet weak var wordsMissedLabel: UILabel!
+    @IBOutlet weak var wordsInGameLabel: UILabel!
+    
+    @IBOutlet weak var wordBeingReviewedLabel: UILabel!
+    
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessedLetterTextField: UITextField!
+    
+    @IBOutlet weak var gameStatusMessageLabel: UILabel!
+    
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
+    @IBOutlet weak var playAgainButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
 
 
 }
